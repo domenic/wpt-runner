686e273b3e1a706e2a72e02e6d26bff85050fe8a
Domenic Denicola <d@domenic.me>
2015-10-05 17:38:26 -0400

    Have the server unref itself when using the API

--- bin/wpt-runner.js
+++ bin/wpt-runner.js
@@ -4,11 +4,7 @@ const path = require("path");
 const wptRunner = require("../lib/wpt-runner.js");
 const consoleReporter = require("../lib/console-reporter.js");
 
-const server = wptRunner(path.resolve(process.argv[2]), consoleReporter);
-
-// Don't keep the server alive.
-server.unref();
-process.on("exit", () => server.close());
+wptRunner(path.resolve(process.argv[2]), consoleReporter);
 
 
 // TODO: failures need to contribute to exit code.
