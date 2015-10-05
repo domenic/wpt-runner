be7fffe0528bab417b6916724e439f11648fbe7c
Domenic Denicola <d@domenic.me>
2015-10-05 18:31:37 -0400

    Count failures and use it as the exit code/promise fulfillment

--- bin/wpt-runner.js
+++ bin/wpt-runner.js
@@ -5,7 +5,10 @@ const wptRunner = require("../lib/wpt-runner.js");
 
 const testsPath = process.argv[2];
 const setup = require(path.resolve(process.argv[3]));
-wptRunner(testsPath, setup);
 
-
-// TODO: failures need to contribute to exit code.
+wptRunner(testsPath, setup)
+  .then(failures => process.exit(failures))
+  .catch(e => {
+    console.error(e.stack);
+    process.exit(1);
+  });
