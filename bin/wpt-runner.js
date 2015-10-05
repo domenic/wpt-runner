cf191becb0e019d526c5a561057ec138d11643c4
Domenic Denicola <d@domenic.me>
2015-10-05 17:55:13 -0400

    Add setup scripts

--- bin/wpt-runner.js
+++ bin/wpt-runner.js
@@ -2,9 +2,10 @@
 "use strict";
 const path = require("path");
 const wptRunner = require("../lib/wpt-runner.js");
-const consoleReporter = require("../lib/console-reporter.js");
 
-wptRunner(process.argv[2], consoleReporter);
+const testsPath = process.argv[2];
+const setup = require(path.resolve(process.argv[3]));
+wptRunner(testsPath, setup);
 
 
 // TODO: failures need to contribute to exit code.
