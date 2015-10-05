e4e10b7d310e3c6467237ced98a054b35b5bd3c7
Domenic Denicola <d@domenic.me>
2015-10-05 17:39:13 -0400

    No need to path.resolve that

--- bin/wpt-runner.js
+++ bin/wpt-runner.js
@@ -4,7 +4,7 @@ const path = require("path");
 const wptRunner = require("../lib/wpt-runner.js");
 const consoleReporter = require("../lib/console-reporter.js");
 
-wptRunner(path.resolve(process.argv[2]), consoleReporter);
+wptRunner(process.argv[2], consoleReporter);
 
 
 // TODO: failures need to contribute to exit code.
