37570d193d4b6634a0bb90e752b62ad80144ed35
Domenic Denicola <d@domenic.me>
2019-03-13 17:37:02 -0400

    Simplify

--- test/runner.js
+++ test/runner.js
@@ -2,21 +2,12 @@
 
 const path = require("path");
 const wptRunner = require("..");
-const { updateTestcaseList } = require("./util");
 
-const listFilePath = path.join(__dirname, "./testcases.json");
 const testsDirPath = path.join(__dirname, "./tests/");
 
-(async () => {
-
-  const testcases = await updateTestcaseList(listFilePath, testsDirPath);
-  const filter = htmlPath => (testcases[htmlPath] === true);
-
-  wptRunner(testsDirPath, { filter })
-    .then(failure => process.exit(failure))
-    .catch(e => {
-      console.error(e.stack);
-      process.exit(1);
-    });
-
-})();
+wptRunner(testsDirPath)
+  .then(failure => process.exit(failure))
+  .catch(e => {
+    console.error(e.stack);
+    process.exit(1);
+  });
